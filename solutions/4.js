a7a450c74136698f66753aa78df24f74b49e3bc8
Sophia <sophiaaa@llip.life>
2017-10-14 16:39:02 -0700

    update problem4

--- solutions/4.js
+++ solutions/4.js
@@ -2,10 +2,8 @@ const jump = (str, num, newstr='', i=0)=>{
   if (i==num){
     return newstr;
   }
-  console.log(str);
-  newstr = newstr + str;
-  return jump(str, num, newstr, i+1);
-}
+  return jump(str, num, newstr + str, i+1);
+};
 module.exports = jump;
 
 
